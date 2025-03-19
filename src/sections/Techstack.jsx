61e95ffca7760a97e32a3d18a93ebcec87b2d06c
Renreal <renreal.dev@gmail.com>
2025-03-19 20:25:35 +0800

    carousel adjustment

--- src/sections/Techstack.jsx
+++ src/sections/Techstack.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { FaReact,FaCss3,FaHtml5,FaBootstrap,FaDocker} from "react-icons/fa";
 import { RiSupabaseLine, RiFirebaseLine } from "react-icons/ri";
 import { GoDatabase } from "react-icons/go";
-import { SiKubernetes, SiGithubactions, SiArgo, SiPostman, SiRancher } from "react-icons/si"
+import { SiKubernetes, SiGithubactions, SiArgo, SiPostman, SiRancher,SiPostgresql } from "react-icons/si"
 import PandaComponent from "./PandaComponent.jsx";
 import CatComponent from "./CatComponent.jsx";
 const FetchSkillsIcon = () => {
@@ -11,24 +11,19 @@ const FetchSkillsIcon = () => {
     <div className="skillsContainer">
       
       <div className="right-container">
-      <span>Tools and technologies I'm familiar with (but not limited to)
+      <span>TOOLS AND TECHNOLOGIES
       </span>
       <div className="right-container-title-icon">
-      <p>Databases and API Connectivity Testing</p>
         <div>
-          <FaReact />
-          <FaHtml5 />
-          <FaCss3 />
-          <FaBootstrap />
-          <RiSupabaseLine />
+          <div className="tech-container">
+          <span>Supabasei <a><RiSupabaseLine /></a></span>
+          <span>Supabasei <a><RiSupabaseLine /></a></span>
+          
+          </div>
           <RiFirebaseLine />
           <GoDatabase />
-          <FaDocker />
-          <SiKubernetes />
-          <SiGithubactions />
-          <SiArgo />
-          <SiPostman />
-          <SiRancher />
+          <SiPostgresql/>
+          <SiPostman/>
         </div>
        </div>
 
