d6bc84863e712ff2828c83867a541f9921ab298d
Renreal <renreal.dev@gmail.com>
2025-03-19 23:01:18 +0800

    create tech-stack component

--- src/sections/Techstack.jsx
+++ src/sections/Techstack.jsx
@@ -1,29 +1,37 @@
 import React from "react";
-import { FaReact,FaCss3,FaHtml5,FaBootstrap,FaDocker} from "react-icons/fa";
+import { FaReact,FaCss3,FaHtml5,FaGithub,FaBootstrap,FaDocker, FaJava} from "react-icons/fa";
 import { RiSupabaseLine, RiFirebaseLine } from "react-icons/ri";
 import { GoDatabase } from "react-icons/go";
-import { SiKubernetes, SiGithubactions, SiArgo, SiPostman, SiRancher,SiPostgresql } from "react-icons/si"
+import { SiKubernetes, SiGithubactions, SiArgo, SiPostman, SiRancher,SiPostgresql,SiJavascript } from "react-icons/si"
 import PandaComponent from "./PandaComponent.jsx";
 import CatComponent from "./CatComponent.jsx";
 const FetchSkillsIcon = () => {
   return (
     
     <div className="skillsContainer">
-      
+      <div className="l-container">work experience section</div>
       <div className="right-container">
-      <span>TOOLS AND TECHNOLOGIES
+      <span className="r-container-title">TOOLS AND TECHNOLOGIES
       </span>
       <div className="right-container-title-icon">
         <div>
           <div className="tech-container">
-          <span>Supabasei <a><RiSupabaseLine /></a></span>
-          <span>Supabasei <a><RiSupabaseLine /></a></span>
+          <span>Supabase <a><RiSupabaseLine /></a></span>
+          <span>Firebase <a><RiFirebaseLine /></a></span>
+          <span>MySql <a><GoDatabase /></a></span>
+          <span>Postgre<a><SiPostgresql /></a></span>
+          <span>Postman<a><SiPostman /></a></span>
+          <span>Docker<a><FaDocker /></a></span>
+          <span>Kubernetes<a><SiKubernetes /></a></span>
+          <span>Rancher<a><SiRancher /></a></span>
+          <span>ArgoCD<a><SiArgo /></a></span>
+          <span>ReactJS<a><FaReact /></a></span>
+          <span>Bootsrap<a><FaBootstrap /></a></span>
+          <span>Javascript<a><SiJavascript /></a></span>
+          <span>Java<a><FaJava /></a></span>
+          <span>Github Actions<a><SiGithubactions /></a></span>
           
           </div>
-          <RiFirebaseLine />
-          <GoDatabase />
-          <SiPostgresql/>
-          <SiPostman/>
         </div>
        </div>
 
